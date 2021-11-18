a6ebbbbc971ba416826befd6269894300e731a45
schnaidm <schnaidm@oregonstate.edu>
2021-11-18 00:35:08 -0500

    Added dropdowns

--- orders.js
+++ orders.js
@@ -2,6 +2,28 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    function getCustomers(res, mysql, context, complete){
+        mysql.pool.query("SELECT phoneNumber, firstName, lastName, email FROM Customers", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.customers = results;
+            complete();
+        });
+    };    
+
+    function getEmployees(res, mysql, context, complete){
+        mysql.pool.query("SELECT employeeID, firstName, lastName, email FROM Employees", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.employees = results;
+            complete();
+        });
+    };   
+
     function getOrders(res, mysql, context, complete){
         mysql.pool.query("SELECT Orders.orderID, DATE(Orders.date) as date, Customers.firstName as CustomerFirstName, Customers.lastName as CustomerLastName, Orders.totalPrice, Employees.firstName as EmployeeFirstName, Employees.lastName as EmployeeLastName FROM Orders JOIN Customers ON Orders.phoneNumber = Customers.phoneNumber LEFT JOIN Employees ON Orders.employeeID = Employees.employeeID", function(error, results, fields){
             if(error){
@@ -18,9 +40,11 @@ module.exports = function(){
         var context = {};
         var mysql = req.app.get('mysql');
         getOrders(res, mysql, context, complete);
+        getCustomers(res, mysql, context, complete);
+        getEmployees(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 3){
                 res.render('orders', context);
             }
 
--- wine_orders.js
+++ wine_orders.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getWineOrders(res, mysql, context, complete){
-        mysql.pool.query("SELECT orderID, bottleID, quantity FROM Orders_Wines", function(error, results, fields){
+        mysql.pool.query("SELECT Orders_Wines.orderID, Orders_Wines.bottleID, Orders_Wines.quantity, Customers.firstName as firstName, Customers.lastName as lastName, Wines.brand as brand, Wines.type as type FROM Orders_Wines JOIN Wines on Orders_Wines.bottleID = Wines.bottleID JOIN Orders on Orders_Wines.orderID = Orders.orderID JOIN Customers on Orders.phoneNumber = Customers.phoneNumber", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -13,15 +13,41 @@ module.exports = function(){
         });
     };    
 
+    function getWines(res, mysql, context, complete){
+        mysql.pool.query("SELECT bottleID, brand, type, year, price, inventoryAmount, status FROM Wines", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.wines = results;
+            complete();
+        });
+    };    
+
+
+    function getOrders(res, mysql, context, complete){
+        mysql.pool.query("SELECT Orders.orderID, DATE(Orders.date) as date, Customers.firstName as CustomerFirstName, Customers.lastName as CustomerLastName, Orders.totalPrice, Employees.firstName as EmployeeFirstName, Employees.lastName as EmployeeLastName FROM Orders JOIN Customers ON Orders.phoneNumber = Customers.phoneNumber LEFT JOIN Employees ON Orders.employeeID = Employees.employeeID", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.orders = results;
+            complete();
+        });
+    };   
+
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
         getWineOrders(res, mysql, context, complete);
+        getOrders(res, mysql, context, complete);
+        getWines(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 3){
                 res.render('wine_orders', context);
+                
             }
 
         }
@@ -29,6 +55,8 @@ module.exports = function(){
     
     router.post('/', function (req, res) {
         var mysql = req.app.get('mysql');
+        var inventorysql = "UPDATE Wines SET inventoryAmount = inventoryAmount - (SELECT quantity FROM Orders_Wines join Wines on Orders_Wines.bottleID = Wines.bottleID WHERE Orders_Wines.orderID = ? and Wines.bottleID = ?) where Wines.bottleID = ?";
+        var inventoryinserts = [req.body.orderID, req.body.bottleID, req.body.bottleID];
         var ordersql = "UPDATE Orders SET totalPrice = (SELECT sum(Wines.price) FROM Orders_Wines join Wines on Orders_Wines.bottleID = Wines.bottleID WHERE Orders_Wines.orderID = ?) Where orderID = ?";
         var orderinserts = [req.body.orderID, req.body.orderID];
         var sql = "INSERT INTO Orders_Wines (orderID, bottleID, quantity) VALUES (?,?,?)";
@@ -43,7 +71,14 @@ module.exports = function(){
                         res.write(JSON.stringify(error));
                         res.end();
                     } else {
-                        res.redirect('/wine_orders');
+                        inventorysql = mysql.pool.query(inventorysql, inventoryinserts, function (error, results, fields) {
+                            if (error) {
+                                res.write(JSON.stringify(error));
+                                res.end();
+                            } else {
+                                res.redirect('/wine_orders');
+                            }
+                        });
                     }
                 });
             }
