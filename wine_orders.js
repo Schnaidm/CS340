d9a5fad2213b4e39941dc7218a43554114d0cb48
schnaidm <schnaidm@oregonstate.edu>
2021-11-17 20:23:15 -0500

    updated wine_orders submit to update orders totalprice

--- wine_orders.js
+++ wine_orders.js
@@ -29,6 +29,8 @@ module.exports = function(){
     
     router.post('/', function (req, res) {
         var mysql = req.app.get('mysql');
+        var ordersql = "UPDATE Orders SET totalPrice = (SELECT sum(Wines.price) FROM Orders_Wines join Wines on Orders_Wines.bottleID = Wines.bottleID WHERE Orders_Wines.orderID = ?) Where orderID = ?";
+        var orderinserts = [req.body.orderID, req.body.orderID];
         var sql = "INSERT INTO Orders_Wines (orderID, bottleID, quantity) VALUES (?,?,?)";
         var inserts = [req.body.orderID, req.body.bottleID, req.body.quantity];
         sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
@@ -36,9 +38,17 @@ module.exports = function(){
                 res.write(JSON.stringify(error));
                 res.end();
             } else {
-                res.redirect('/wine_orders');
+                ordersql = mysql.pool.query(ordersql, orderinserts, function (error, results, fields) {
+                    if (error) {
+                        res.write(JSON.stringify(error));
+                        res.end();
+                    } else {
+                        res.redirect('/wine_orders');
+                    }
+                });
             }
         });
+        
     });    
 
     return router;
